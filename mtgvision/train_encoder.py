f16b08c8675f40fc87fafa6fd7445d3372099c0a
Nathan Michlo <nathanjmichlo@gmail.com>
2025-03-23 11:17:19 +0200

    run 6.3.b

--- mtgvision/train_encoder.py
+++ mtgvision/train_encoder.py
@@ -674,14 +674,14 @@ class Config(pydantic.BaseModel):
     y_size_hw: tuple[int, int] = (192, 128)
     # optimisation
     optimizer: Literal["adam", "radam"] = "adam"
-    learning_rate: float = 5e-5
+    learning_rate: float = 5e-4
     weight_decay: float = 1e-9  # hurts performance if < 1e-7, e.g. 1e-5 is really bad
-    batch_size: int = 16
+    batch_size: int = 12
     gradient_clip_val: float = 0.5
     accumulate_grad_batches: int = 4
     # loss
     loss_recon: Optional[str] = "ssim5+l1"  # 'ssim5+l1'
-    loss_contrastive: Optional[str] = "none"  # ntxent
+    loss_contrastive: Optional[str] = "ntxent"  # ntxent
     loss_contrastive_batched: bool = False
     scale_loss_recon: float = 1
     scale_loss_contrastive: float = 1
@@ -709,8 +709,8 @@ if __name__ == "__main__":
             "--prefix=cnxt2",
             # "--checkpoint=/home/nmichlo/workspace/mtg/mtg-vision/mtgvision_encoder/6__dvea3b14/checkpoints/epoch=0-step=67500.ckpt",
             # "--checkpoint=/home/nmichlo/workspace/mtg/mtg-vision/mtgvision_encoder/6.2__o0yxl20m/checkpoints/epoch=0-step=125000.ckpt",
-            # "--checkpoint=/home/nmichlo/workspace/mtg/mtg-vision/mtgvision_encoder/6.2__5u5qqmvz/checkpoints/epoch=0-step=217500.ckpt",
-            "--checkpoint=/home/nmichlo/workspace/mtg/mtg-vision/mtgvision_encoder/6.3__81rawzyz/checkpoints/epoch=0-step=365000.ckpt",
+            "--checkpoint=/home/nmichlo/workspace/mtg/mtg-vision/mtgvision_encoder/6.2__5u5qqmvz/checkpoints/epoch=0-step=217500.ckpt",
+            # "--checkpoint=/home/nmichlo/workspace/mtg/mtg-vision/mtgvision_encoder/6.3__81rawzyz/checkpoints/epoch=0-step=365000.ckpt",
         ]
     )
     _cli()
