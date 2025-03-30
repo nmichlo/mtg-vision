2d1ebfc876c04dc00c2543575a784c046e4596fc
Nathan Michlo <nathanjmichlo@gmail.com>
2025-03-30 21:55:42 +0200

    validate

--- mtgvision/encoder_validate.py
+++ mtgvision/encoder_validate.py
@@ -4,6 +4,8 @@ Embed images into qdrant and query them to validate models are working correctly
 
 import itertools
 import time
+
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from doorway.x import ProxyDownloader
@@ -15,25 +17,27 @@ from mtgvision.qdrant_populate import VectorStoreQdrant
 from mtgvision.util.image import imread_float
 
 
-def _cli():
+def _cli(virtual: bool = False):
     encoder = CoreMlEncoder(MODEL_PATH.with_suffix(".encoder.mlpackage"))
 
     dataset = RanMtgEncDecDataset(default_batch_size=1)
 
     proxy = ProxyDownloader()
 
-    # db = VectorStoreQdrant()
     db = VectorStoreQdrant()
 
     # 2. check accuracy
     def _yield_virtual_points():
         for card in tqdm(dataset.mtg.card_iter(), total=len(dataset.mtg)):
             card.download(proxy=proxy)
-            x = SyntheticBgFgMtgImages.make_virtual(
-                imread_float(card.img_path),
-                imread_float(dataset.ilsvrc.ran_path()),
-                size_hw=dataset.x_size_hw,
-            )
+            orig = imread_float(card.img_path)
+            if not virtual:
+                x = SyntheticBgFgMtgImages.make_cropped(orig, size_hw=dataset.x_size_hw)
+            else:
+                bg = imread_float(dataset.ilsvrc.ran_path())
+                x = SyntheticBgFgMtgImages.make_virtual(
+                    orig, bg, size_hw=dataset.x_size_hw
+                )
             z = encoder.predict(x).tolist()
             yield z, card
 
@@ -51,11 +55,15 @@ def _cli():
 
     t = time.time()
     for i, (z, card) in enumerate(itertools.islice(_yield_virtual_points(), N)):
-        results = db.query(z, k=5)
+        results = db.query_nearby(z, k=5)
         if str(card.id) == results[0].id:
             top_1_correct += 1
         if str(card.id) in {point.id for point in results}:
             top_5_correct += 1
+        else:
+            print(card)
+            plt.imshow(imread_float(card.img_path))
+            plt.show()
         if time.time() - t > 2:
             t = time.time()
             _print_correct()
