24839b6480fdb09ddfd95bcffd4533c559b02910
Nathan Michlo <nathanjmichlo@gmail.com>
2025-04-17 16:19:47 +0200

    export nobuco tfjs

--- mtgvision/encoder_export.py
+++ mtgvision/encoder_export.py
@@ -223,7 +223,7 @@ def _cli() -> None:
     args = parser.parse_args()
 
     if not args.export:
-        args.export = ["coreml"]
+        args.export = ["tfjs"]
 
     print(f"Exporting {args.path} to all of: {args.export}")
     _export(
